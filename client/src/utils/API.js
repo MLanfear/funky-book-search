e8843966a84ba996962174b83b0152983cf32df4
MLanfear <maxjlanfear@gmail.com>
2022-12-31 00:45:14 -0800

    almost theregit add -A

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,8 +1,9 @@
 // OLD API ROUTES
 
-
 // make a search to google books api
 // https://www.googleapis.com/books/v1/volumes?q=harry+potter
-export const searchGoogleBooks = (query) => {
+const searchGoogleBooks = (query) => {
   return fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
 };
+
+module.exports = searchGoogleBooks;
